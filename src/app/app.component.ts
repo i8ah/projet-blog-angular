14956044ae10597aa69ac10de31e4f9d02175fcc
Ibrahima Camara <ibrahima.s.camara@gmail.com>
2018-12-26 21:46:30 +0100

    commit all

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'projet-blog-angular';
+
+  posts = [
+
+   new Post('Mon premier post',
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae qui perferendis, doloremque eveniet a doloribus repudiandae dicta autem dolorum aliquam recusandae architecto tempore repellendus id temporibus, rerum. Tempora, magnam, ipsa!',
+      1)
+  ,
+
+  new Post('Mon deuxième post',
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae qui perferendis, doloremque eveniet a doloribus repudiandae dicta autem dolorum aliquam recusandae architecto tempore repellendus id temporibus, rerum. Tempora, magnam, ipsa!',
+      -1)
+
+  ,
+
+  new Post('Encore un post',
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae qui perferendis, doloremque eveniet a doloribus repudiandae dicta autem dolorum aliquam recusandae architecto tempore repellendus id temporibus, rerum. Tempora, magnam, ipsa!',
+      0)
+
+  ];
+
+}
+
+
+class Post {
+
+  title: string;
+  content: string;
+  loveIts: number;
+  created_at: Date;
+
+  constructor(title, content, loveIts) {
+    this.title = title;
+    this.content = content;
+    this.loveIts = loveIts;
+    this.created_at = new Date();
+  }
+
 }
